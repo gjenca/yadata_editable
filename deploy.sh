2b99cc465042d0619a7ee7c1ffa7021a248c2273
Gejza Jenča <gejza.jenca@stuba.sk>
2023-07-09 14:27:10 +0200

    data, deploy

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# vim: et ts=4 sw=4 ai
+
+PROJECT=yadata_editable
+#STATICDIR=/data/www/$PROJECT-static
+CODEDIR=/usr/local/lib/$PROJECT
+
+
+rm -r $CODEDIR
+#rm -r $STATICDIR
+mkdir -pv $CODEDIR
+mkdir -pv $CODEDIR/template
+#mkdir -pv $STATICDIR/static
+cp -rvf template $CODEDIR/template
+cp -vf main.py $CODEDIR
+chown -R www-data:www-data $CODEDIR
+chmod -R u=rX,g=,o= $CODEDIR
+find  $CODEDIR -type d -exec chmod -v u+w {} \;
+#systemctl restart $PROJECT
+#systemctl status $PROJECT
--- main.py
+++ main.py
@@ -1,49 +1,94 @@
 #!/usr/bin/env python3
 import sys
-from flask import Flask,abort,request,redirect
-from jinja2 import Environment,FileSystemLoader
+import os
+import tempfile
+import shutil
+import socket
+
 import yaml
+from flask import Flask,abort,request,redirect,flash,url_for,Response
+from jinja2 import Environment,FileSystemLoader
 app = Flask(__name__)
+app.secret_key='pb3wuD31NCwnQ0CQP4rUAZ/x0OU'
 
+if socket.gethostname()=='www-kmadg':
+    DATADIR='/var/lib/ssaos_abstracts'
+    TEMPLATE_DIR='/usr/local/lib/yadata_editable/template'
+else:
+    DATADIR='./data'
+    TEMPLATE_DIR='./template'
 
-DATADIR='./data'
-env=Environment(loader=FileSystemLoader('./template'),
+env=Environment(loader=FileSystemLoader(TEMPLATE_DIR),
     line_statement_prefix='#',
     extensions=['jinja2.ext.loopcontrols'],
 )
 
-@app.route('/thanks')
-def thanks():
-        t=env.get_template('thanks.html')
-        return t.render()
+@app.route('/abstract/<objid>')
+def abstract(objid):
+    abstract_filename=f'{DATADIR}/{objid}/abstract.tex'
+    try:
+        with open(abstract_filename) as f:
+            abstract=f.read()
+    except FileNotFoundError:
+        abort(404)
+    return Response(abstract,content_type='text/plain')
 
-@app.route('/data/<objid>',methods=["GET","POST"])
-def editable_dict(objid):
+@app.route('/thanks/<objid>')
+def thanks(objid):
 
     yaml_fnm=f'{DATADIR}/{objid}/data.yaml'
-    abstract_filename=f'{DATADIR}/{objid}/abstract.pdf'
+
+    abstract_filename=f'{DATADIR}/{objid}/abstract.tex'
     try:
         with open(yaml_fnm) as f:
             obj=yaml.load(f,Loader=yaml.Loader)
     except FileNotFoundError:
         abort(404)
-    if request.method=='GET':
-        t=env.get_template('form.html')
-        return t.render(obj=obj,action=request.url)
-    else:
-        if request.method=='POST':
-            print(request.form,file=sys.stderr)
-            for name in request.form:
-                if name in obj:
-                    obj[name]=request.form[name]
-            with open(yaml_fnm,mode='w') as f:
-                f.write(yaml.dump(obj))
-            if 'abstract' in request.files:
-                file=request.files['abstract']
-                if not file.filename=='':
+    try:
+        st=os.stat(abstract_filename)
+        have_abstract=True
+        abstract_length=st.st_size
+    except FileNotFoundError:
+        have_abstract=False
+        abstract_length=-1
+    t=env.get_template('thanks.html')
+    return t.render(obj=obj,have_abstract=have_abstract,abstract_length=abstract_length,
+                    correct_url=url_for('editable_dict',objid=objid),
+                    abstract_url=url_for('abstract',objid=objid),
+                    )
+
+@app.route('/data/<objid>',methods=["GET","POST"])
+def editable_dict(objid):
+
+    error=None
+    yaml_fnm=f'{DATADIR}/{objid}/data.yaml'
+    abstract_filename=f'{DATADIR}/{objid}/abstract.tex'
+    t=env.get_template('form.html')
+    try:
+        with open(yaml_fnm) as f:
+            obj=yaml.load(f,Loader=yaml.Loader)
+    except FileNotFoundError:
+        abort(404)
+    obj['abstract_uploaded']=False
+    if request.method=='POST':
+        print(request.form,file=sys.stderr)
+        for name in request.form:
+            if name in obj:
+                obj[name]=request.form[name]
+        if 'abstract_tex' in request.files:
+            file=request.files['abstract_tex']
+            if not file.filename=='':
+                if not file.filename.lower().endswith('.tex'):
+                    error=f'{file.filename} does not appear to be a LaTeX file'
+                    return t.render(obj=obj,action=request.url,error=error)
+                else:
+                    obj['abstract_uploaded']=True
                     file.save(abstract_filename)
-            return redirect('/thanks')
-
-
-app.run()
+        f=tempfile.NamedTemporaryFile(delete=False,mode='w')
+        f.write(yaml.dump(obj))
+        f.close()
+        shutil.move(f.name,yaml_fnm)
+        return redirect(url_for('thanks',objid=objid))
+    return t.render(obj=obj,action=request.url,error=error)
+#app.run()
 
