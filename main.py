71b5ae6ad4c7aca46e5167b304b79b791dac3744
Gejza Jenča <gejza.jenca@stuba.sk>
2023-07-10 20:30:09 +0200

    Logika vo formulari

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ import unicodemail
 import yaml
 from flask import Flask,abort,request,redirect,flash,url_for,Response
 from jinja2 import Environment,FileSystemLoader
+
+def construct_yaml_str(self, node):
+    return self.construct_scalar(node)
+
+# Override the default string handling function
+# to always return unicode objects
+yaml.Loader.add_constructor('tag:yaml.org,2002:str', construct_yaml_str)
+yaml.SafeLoader.add_constructor('tag:yaml.org,2002:str', construct_yaml_str)
+
+def unicode_representer(dumper, uni):
+    node = yaml.ScalarNode(tag='tag:yaml.org,2002:str', value=uni)
+    return node
+
+# This is necessary to dump ASCII string normally
+yaml.add_representer(str, unicode_representer)
+
 app = Flask(__name__)
 app.secret_key='pb3wuD31NCwnQ0CQP4rUAZ/x0OU'
 
@@ -88,6 +104,13 @@ def editable_dict(objid):
             obj=yaml.load(f,Loader=yaml.Loader)
     except FileNotFoundError:
         abort(404)
+    try:
+        st=os.stat(abstract_filename)
+        have_abstract=True
+        abstract_length=st.st_size
+    except FileNotFoundError:
+        have_abstract=False
+        abstract_length=-1
     obj['abstract_uploaded']=False
     if request.method=='POST':
         print(request.form,file=sys.stderr)
@@ -104,10 +127,16 @@ def editable_dict(objid):
                     obj['abstract_uploaded']=True
                     file.save(abstract_filename)
         f=tempfile.NamedTemporaryFile(delete=False,mode='w')
-        f.write(yaml.dump(obj))
+        f.write(yaml.dump(obj,allow_unicode=True))
         f.close()
         shutil.move(f.name,yaml_fnm)
         return redirect(url_for('thanks',objid=objid))
-    return t.render(obj=obj,action=request.url,error=error)
+    return t.render(obj=obj,
+                    error=error,
+                    have_abstract=have_abstract,
+                    abstract_length=abstract_length,
+                    abstract_url=url_for('abstract',objid=objid)
+                    )
+
 #app.run()
 
